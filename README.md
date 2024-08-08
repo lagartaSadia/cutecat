53cc4efba5592f493ab821a0e71ddb0b81ca9ef4
lagartaSadia <campolino.l@gmail.com>
2024-08-08 19:24:24 -0300

    Adiciona informações ao README.md e faz pequenos comentarios ao script.js

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Pequeno site para testar e treinar o uso de API</h1>
+
+<p>Usei esse site para testar o uso de APIs.
+Ao abrir o site você é recepcionado por um gatinho fofinho</p>
--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// Captura a TAG img do html
 const imagem = document.querySelector('img')
 
+// Função assincrona que busca a image de um gato
+// na API cataas
 async function getCatPicture() {
+  // Fecth é usado para capturar o retorno da API
   await fetch("https://cataas.com/cat")
   .then(res => {
+    // Adiciona o link da foto na TAG img
     imagem.src = `${res.url}`
   })
 }
 
+// Executa função assincrona
 getCatPicture()
